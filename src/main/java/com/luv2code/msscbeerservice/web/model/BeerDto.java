0bb4d06995aa4335e494a1b2cd0ce37144f9162b
JackTran791 <congsonit@gmail.com>
2020-01-13 14:26:22 -0600

    - Add Jackson Configuration to BeerDto. Closes #13.

--- src/main/java/com/luv2code/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/luv2code/msscbeerservice/web/model/BeerDto.java
@@ -1,14 +1,12 @@
 package com.luv2code.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -27,9 +25,11 @@ public class BeerDto {
     @Null
     private Integer version;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 
@@ -43,6 +43,7 @@ public class BeerDto {
     @Positive
     private long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @NotNull
     @Positive
     private BigDecimal price;
