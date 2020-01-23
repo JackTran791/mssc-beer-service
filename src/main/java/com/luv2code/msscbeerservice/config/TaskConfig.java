6007877f98712e6c95be115220ffbd6232b6fc0e
JackTran791 <congsonit@gmail.com>
2020-01-23 13:48:22 -0600

    - Implement Brewing Service to trigger Brewing. Closes #28.

---
+++ src/main/java/com/luv2code/msscbeerservice/config/TaskConfig.java
@@ -0,0 +1,22 @@
+package com.luv2code.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Jack Tran
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class TaskConfig {
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
